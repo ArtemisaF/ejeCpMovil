6cac4b5c6491d2eee979d2168e56c9a78ded7afe
Ocrezzu <davidjrf95@gmail.com>
2020-12-15 07:35:37 -0500

    fin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,15 +9,28 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from "angularfire2";
+import { environment } from '../environments/environment';
+import { AngularFirestoreModule, AngularFirestore } from
+"angularfire2/firestore"
+import {AngularFireStorage} from '@angular/fire/storage';
+
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+  AngularFireModule.initializeApp(environment.firebaseConfig),
+  AngularFireModule, AngularFirestoreModule],
   providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+  StatusBar,
+   
+  AngularFirestoreModule,
+  AngularFirestore,
+  AngularFireStorage,
+  SplashScreen,
+  { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
-})
-export class AppModule {}
+  })
+  export class AppModule { }
